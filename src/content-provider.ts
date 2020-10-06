1194253cfa3b50fcde20102b9f3604f6269b3127
Dain Cilke <dain.cilke@gmail.com>
2020-10-06 17:43:49 +0200

    now with syntax highlighting

--- src/content-provider.ts
+++ src/content-provider.ts
@@ -1,10 +1,51 @@
 import * as vscode from 'vscode'
 
+// Shamelessly ripped from Stack Overflow
+function syntaxHighlight(json) {
+  if (typeof json != 'string') {
+    json = JSON.stringify(json, undefined, 2)
+  }
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      var cls = 'number'
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'key'
+        } else {
+          cls = 'string'
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'boolean'
+      } else if (/null/.test(match)) {
+        cls = 'null'
+      }
+      return '<span class="' + cls + '">' + match + '</span>'
+    }
+  )
+}
+
 export class GroqContentProvider implements vscode.TextDocumentContentProvider {
   private html: string = ''
+  // pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
+  private css: string = `
+.string { color: green; }
+.number { color: darkorange; }
+.boolean { color: blue; }
+.null { color: magenta; }
+.key { color: red; }
+`
 
   constructor(data: any) {
-    this.html = `<html><body><pre>${JSON.stringify(data, null, 2)}</pre></body></html>`
+    this.html = `<html>
+<head>
+    <style>
+    ${this.css}
+    </style
+</head>
+<body><pre>${syntaxHighlight(data)}</pre></body></html>
+`
   }
 
   provideTextDocumentContent(_: vscode.Uri): vscode.ProviderResult<string> {
