184e93d53e7033d73ad7ad5681d4c7fafc96d7d7
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-10-06 21:56:13 +0200

    Reuse active panel if one is open

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as path from 'path'
 import * as vscode from 'vscode'
-import {promises as fs} from 'fs'
 import {loadConfig} from './config/findConfig'
 import {executeGroq} from './query'
 import {GroqContentProvider} from './content-provider'
@@ -22,6 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
     registerCodeLens()
   }
 
+  let activePanel
   let disposable = vscode.commands.registerCommand('extension.executeGroq', async () => {
     const activeTextEditor = vscode.window.activeTextEditor
     if (!activeTextEditor) {
@@ -38,14 +38,19 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage(
       `Using projectId "${config.projectId}" and dataset "${config.dataset}"`
     )
-    const file = await fs.readFile(activeFile, 'utf8')
-    const result = await executeGroq(config.projectId, config.dataset, file)
-    const panel = vscode.window.createWebviewPanel(
-      'executionResultsWebView',
-      'GROQ Execution Result',
-      vscode.ViewColumn.Two,
-      {}
-    )
+
+    const content = activeTextEditor.document.getText()
+    const result = await executeGroq(config.projectId, config.dataset, content)
+
+    if (!activePanel) {
+      activePanel = vscode.window.createWebviewPanel(
+        'executionResultsWebView',
+        'GROQ Execution Result',
+        vscode.ViewColumn.Beside,
+        {}
+      )
+    }
+
     const contentProvider = new GroqContentProvider(result)
     const registration = vscode.workspace.registerTextDocumentContentProvider(
       'groq',
@@ -54,8 +59,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(registration)
 
     const html = await contentProvider.getCurrentHTML()
-    panel.webview.html = html
-    // openInUntitled(JSON.stringify(result, null, 2), 'json')
+    activePanel.webview.html = html
   })
 
   context.subscriptions.push(disposable)
