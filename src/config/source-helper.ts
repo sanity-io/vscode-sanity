7ee0199a36b5adaabea39ff2defc1af8dc4d8675
Dain Cilke <dain.cilke@gmail.com>
2020-10-07 16:05:32 +0200

    restrict codelens groq regex

--- src/config/source-helper.ts
+++ src/config/source-helper.ts
@@ -9,11 +9,11 @@ export interface ExtractedTemplateLiteral {
 export function extractAllTemplateLiterals(document: TextDocument): ExtractedTemplateLiteral[] {
   const documents: ExtractedTemplateLiteral[] = []
   const text = document.getText()
-  const regExpGQL = new RegExp("\\s*(`|')([\\s\\S]+?)(`|')", 'mg')
+  const regExpGQL = new RegExp('\\s*groq`([\\s\\S]+?)`', 'mg')
 
   let result
   while ((result = regExpGQL.exec(text)) !== null) {
-    const content = result[2]
+    const content = result[1]
     documents.push({
       content: content,
       uri: document.uri.path,
