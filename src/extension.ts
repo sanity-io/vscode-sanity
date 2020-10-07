4c9ec4595e96887cd3f1f0c4373c240c70135559
j33ty <radhe@sanity.io>
2020-10-07 17:54:29 +0530

    Execute GROQ: Add params support

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 import * as path from 'path'
 import * as vscode from 'vscode'
 import {loadConfig} from './config/findConfig'
-import {executeGroq} from './query'
+import {promises as fs} from 'fs'
+import {executeGroq, executeGroqWithParams} from './query'
 import {GroqContentProvider} from './content-provider'
 import {GROQCodeLensProvider} from './config/groq-codelens-provider'
 
@@ -23,24 +24,26 @@ export function activate(context: vscode.ExtensionContext) {
 
   let activePanel
   let disposable = vscode.commands.registerCommand('extension.executeGroq', async () => {
-    const activeTextEditor = vscode.window.activeTextEditor
-    if (!activeTextEditor) {
-      vscode.window.showErrorMessage('Nothing to execute')
+    let files
+    try {
+      files = await readRequiredFiles(false)
+    } catch (err) {
+      vscode.window.showErrorMessage(err.message)
       return
     }
-    const activeFile = vscode.window.activeTextEditor?.document.fileName || ''
-    const activeDir = path.dirname(activeFile)
-    const config = await loadConfig(activeDir)
-    if (!config) {
-      vscode.window.showErrorMessage('Could not resolve sanity.json configuration file')
-      return
-    }
+
     vscode.window.showInformationMessage(
-      `Using projectId "${config.projectId}" and dataset "${config.dataset}"`
+      `Using projectId "${files.config.projectId}" and dataset "${files.config.dataset}"`
     )
 
-    const content = activeTextEditor.document.getText()
-    const result = await executeGroq(config.projectId, config.dataset, content)
+    // FIXME: Throw error object in webview?
+    let result
+    try {
+      result = await executeGroq(files.config.projectId, files.config.dataset, files.groq)
+    } catch (err) {
+      vscode.window.showErrorMessage(err)
+      return
+    }
 
     if (!activePanel) {
       activePanel = vscode.window.createWebviewPanel(
@@ -51,24 +54,92 @@ export function activate(context: vscode.ExtensionContext) {
       )
     }
 
-    const contentProvider = new GroqContentProvider(result)
-    const registration = vscode.workspace.registerTextDocumentContentProvider(
-      'groq',
-      contentProvider
-    )
-    context.subscriptions.push(registration)
-
+    const contentProvider = await registereContentProvider(context, result)
     const html = await contentProvider.getCurrentHTML()
     activePanel.webview.html = html
   })
+  context.subscriptions.push(disposable)
 
+  disposable = vscode.commands.registerCommand('extension.executeGroqWithParams', async () => {
+    let files
+    try {
+      files = await readRequiredFiles(true)
+    } catch (err) {
+      vscode.window.showErrorMessage(err.message)
+      return
+    }
+
+    vscode.window.showInformationMessage(
+      `Using projectId "${files.config.projectId}" and dataset "${files.config.dataset}"`
+    )
+
+    // FIXME: Throw error object in webview?
+    let result
+    try {
+      result = await executeGroqWithParams(files.config.projectId, files.config.dataset, files.groq, files.params)
+    } catch (err) {
+      vscode.window.showErrorMessage(err)
+      return
+    }
+
+    if (!activePanel) {
+      activePanel = vscode.window.createWebviewPanel(
+        'executionResultsWebView',
+        'GROQ Execution Result',
+        vscode.ViewColumn.Beside,
+        {}
+      )
+    }
+
+    const contentProvider = await registereContentProvider(context, result)
+    const html = await contentProvider.getCurrentHTML()
+    activePanel.webview.html = html
+  })
   context.subscriptions.push(disposable)
 }
 
-// async function openInUntitled(content: string, language?: string) {
-//   const document = await vscode.workspace.openTextDocument({
-//     language,
-//     content,
-//   })
-//   vscode.window.showTextDocument(document)
-// }
+type config = {
+  projectId: string
+  dataset: string
+}
+
+type RequiredFiles = {
+  config: config
+  groq: string
+  params: string
+}
+
+async function readRequiredFiles(askParamsFile: boolean): Promise<RequiredFiles> {
+  let files = <RequiredFiles>{}
+  const activeFile = vscode.window.activeTextEditor?.document.fileName || ''
+  const activeDir = path.dirname(activeFile)
+  files.config = await loadConfig(activeDir)
+  if (!files.config) {
+    throw new Error('Could not resolve sanity.json configuration file')
+  }
+
+  const activeTextEditor = vscode.window.activeTextEditor
+  if (!activeTextEditor) {
+    throw new Error('Nothing to execute')
+  }
+
+  files.groq = activeTextEditor.document.getText()
+  if (askParamsFile) {
+    const paramsFile = await vscode.window.showInputBox()
+    if (!paramsFile) {
+      throw new Error('Invalid param file received')
+    }
+    files.params = await fs.readFile(activeDir + '/' + paramsFile, 'utf8')
+  }
+  return files
+}
+
+async function registereContentProvider(
+  context: vscode.ExtensionContext,
+  result: any
+): Promise<any> {
+  const contentProvider = new GroqContentProvider(result)
+  const registration = vscode.workspace.registerTextDocumentContentProvider('groq', contentProvider)
+  context.subscriptions.push(registration)
+  return contentProvider
+}
--- src/query.ts
+++ src/query.ts
@@ -1,10 +1,18 @@
 import sanityClient from '@sanity/client'
 
-export async function executeGroq(projectId: string, dataset: string, groq: string): Promise<any> {
-  const client = sanityClient({
+function getClient(projectId: string, dataset: string, useCDN: boolean) {
+  return sanityClient({
     projectId: projectId,
     dataset: dataset,
     useCdn: false,
   })
-  return await client.fetch(groq, {}, {filterResponse: false})
+}
+
+export async function executeGroq(projectId: string, dataset: string, groq: string): Promise<any> {
+  return await getClient(projectId, dataset, false).fetch(groq, {}, {filterResponse: false})
+}
+
+export async function executeGroqWithParams(projectId: string, dataset: string, groq: string, params: string): Promise<any> {
+  const parsedParams = JSON.parse(params)
+  return await getClient(projectId, dataset, false).fetch(groq, parsedParams, {filterResponse: false})
 }
