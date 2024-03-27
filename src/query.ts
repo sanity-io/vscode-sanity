f8006ff773b9ba1e690b7baf744436a6f774944f
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2024-03-26 23:55:35 -0400

    chore: upgrade packages to support latest api versioning

--- src/query.ts
+++ src/query.ts
@@ -1,9 +1,10 @@
-import sanityClient from '@sanity/client'
+import { createClient } from '@sanity/client'
 import * as vscode from 'vscode'
 
 export async function executeGroq(options: {
   projectId: string
   dataset: string
+  apiVersion: string
   query: string
   params: Record<string, unknown>
   useCdn: boolean
@@ -11,13 +12,15 @@ export async function executeGroq(options: {
 }) {
   const {query, params, ...clientOptions} = options
   const {token, ...noTokenClientOptions} = clientOptions
-  return sanityClient(clientOptions)
+  return createClient(clientOptions)
     .fetch(query, params, {filterResponse: false})
     .catch((err) => {
       if (err.statusCode === 401) {
         vscode.window.showInformationMessage(err.message + '. Falling back to public dataset.')
-        return sanityClient(noTokenClientOptions).fetch(query, params, {filterResponse: false})
+        return createClient(noTokenClientOptions).fetch(query, params, {filterResponse: false})
       }
+
+      console.error(err)
 
       throw err
     })
