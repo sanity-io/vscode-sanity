1d08ae9004fbf3b1daedf1e8e94d4f7e3e69a7a2
Kristoffer Brabrand <kristoffer@brabrand.no>
2025-10-20 16:53:26 +0200

    docs(README): Update logo 🎨

--- release.config.mjs
+++ release.config.mjs
@@ -4,7 +4,14 @@
 export default {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { "type": "docs", "scope": "README", "release": "patch" },
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     [
       "semantic-release-vsce",
